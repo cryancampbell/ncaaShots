2408520221b7ac778574d5e7dc71c8f1748cc309
Christopher Campbell <ryan@christosmacbook.lan>
2022-11-10 11:13:57 -0500

    some of the way there, need to test on more games, add OT, and get subs down

--- pullShots.sh
+++ pullShots.sh
@@ -11,29 +11,76 @@ chrome --headless --disable-gpu --dump-dom  `echo "http://gamezone.stats.com/gz3
 
 ## summary file
 #home
-HOME=`cat tmpFiles/tmp.html | grep -A1 '<tr id="homeLineScore">' | tail -n1 | sed 's/.*<td>//g' | sed 's/ <.td>.*//g'`
+HOME=`cat tmp.html | grep -A1 '<tr id="homeLineScore">' | tail -n1 | sed 's/.*<td>//g' | sed 's/ <.td>.*//g'`
 #away
-AWAY=`cat tmpFiles/tmp.html | grep -A1 '<tr id="awayLineScore">' | tail -n1 | sed 's/.*<td>//g' | sed 's/ <.td>.*//g'`
+AWAY=`cat tmp.html | grep -A1 '<tr id="awayLineScore">' | tail -n1 | sed 's/.*<td>//g' | sed 's/ <.td>.*//g'`
 #date
 MATCHUP=`echo $AWAY vs $HOME`
-DATE=`cat tmpFiles/tmp.html | grep $MATCHUP | grep "|" | sed 's/.* | //g' | sed 's/<.p>//g'`
+DATE=`cat tmp.html | grep " vs " | grep "<p>" | grep "|" | sed 's/.* | //g' | sed 's/<.p>//g'`
+DATESC=`echo $DATE | sed 's/,//g'`
+
+DAY=`echo $DATESC | cut -d" " -f2`
+MONTH=`echo $DATESC | cut -d" " -f1`
+YEAR=`echo $DATESC | cut -d" " -f3`
 
 #final score
-HOMESCORE=`cat tmpFiles/tmp.html | grep -m1 -A1 gz_homeTm | tail -n1 | sed 's/.*<div class="gz_tmScore"><span class="mvc_total">//g' | sed 's/<.span>.*//g'`
-AWAYSCORE=`cat tmpFiles/tmp.html | grep -m1 -B2 gz_homeTm | head -n1 | sed 's/.*<div class="gz_tmScore"><span class="mvc_total">//g' | sed 's/<.span>.*//g'`
+HOMESCORE=`cat tmp.html | grep -m1 -A1 gz_homeTm | tail -n1 | sed 's/.*<div class="gz_tmScore"><span class="mvc_total">//g' | sed 's/<.span>.*//g'`
+AWAYSCORE=`cat tmp.html | grep -m1 -B2 gz_homeTm | head -n1 | sed 's/.*<div class="gz_tmScore"><span class="mvc_total">//g' | sed 's/<.span>.*//g'`
+
+echo $GAMENUM,$YEAR,$MONTH,$DAY,$DATESC,$AWAY,$AWAYSCORE,$HOME,$HOMESCORE > summary.csv
+
 
 #find out Neutral?
 
 ## play by play
-#works pretty well, text is a big block though...
-grep Substitution tmpFiles/tmp.html | sed 's/<tr pbpid="." class="shsRow1Row shsPBPRow">/\n/g' \
+grep Substitution tmp.html | sed 's/<tr pbpid="." class="shsRow1Row shsPBPRow">/\n/g' \
 			 | sed 's/<tr pbpid=".." class="shsRow1Row shsPBPRow">/\n/g' \
 			 | sed 's/<tr pbpid="..." class="shsRow1Row shsPBPRow">/\n/g' \
-			 | sed 's/ //g' | sed 's/<trclass="shsColTtlRowshsMorePBPRowmvc_pbpHeader"><td>H<.td><td>Time<.td><td>Team<.td><td>Event<.td>.*<.tr>//g' \
-			 | sed 's/<tdclass="shsNamD">//g' | sed 's/<.td><tdclass="shsTotD">/,/g' \
-			 | sed 's/<.td><.tr>//g' | sed 's/<.td>/,/g' | sed 's/&nbsp;//g' | sed 's/<strong>//g' | sed 's/<.strong>//g'
+			 | sed 's/<tr class="shsColTtlRow shsMorePBPRow mvc_pbpHeader"><td>H<.td><td>Time<.td><td>Team<.td><td>Event<.td>.*<.tr>//g' \
+			 | sed 's/ .*<td class="shsNamD"> .*1st .*<.td> .*<td class="shsTotD">/1st;/g' \
+			 | sed 's/ .*<td class="shsNamD"> .*2nd .*<.td> .*<td class="shsTotD">/2nd;/g' \
+			 | sed 's/<.td>/;/g' | sed 's/<td class="shsNamD">/;/g' \
+			 | sed 's/<.td><.tr>//g' | sed 's/&nbsp;//g' | sed 's/<strong>//g' | sed 's/<.strong>//g' | grep -v "PlayByPlayTable" > pbp.ssv
 
-## box score
+cat pbp.ssv | cut -d";" -f1,2,4,8,10,11 | sed 's/<.tr>//g' | sed 's/ //g' > nonTextPBP.ssv
+cat pbp.ssv | cut -d";" -f6 > textPBP.ssv
+
+paste nonTextPBP.ssv textPBP.ssv | sed 's/	                 //g' | sed 's/;/,/g' > pbp.csv
+
+#add a seconds in column, to sync with shot info, so 20:00 = 0, 19:30 = 30, 10:00 = 600, etc
+PBPLINES=`wc -l pbp.csv | sed 's, pbp.csv,,g' | sed 's, ,,g'`
+rm pbpWsectime.csv
+
+for P in `seq 1 $PBPLINES`; do
+	PLAY=`head -n$P pbp.csv | tail -n1`
+	HALF=`echo $PLAY | cut -d, -f1 | cut -c1`
+	TIME=`echo $PLAY | cut -d, -f2`
+	MIN=`echo $TIME | cut -d: -f1`
+	SEC=`echo $TIME | cut -d: -f2`
+	SECTENS=`echo $TIME | cut -d: -f2 | cut -c1`
+
+	if [[ SECTENS -eq 0 ]]; then
+		SEC=`echo $TIME | cut -d: -f2 | cut -c2`
+	fi
+
+	MININ=$((`echo $((19 - MIN))` * 60))
+	SECIN=$((60 - SEC))
+	### going to need some OT adj here
+	HALFADJ=$((`echo $((HALF - 1))` * 1200))
+
+	SECTIME=$((MININ + SECIN + HALFADJ))
+
+	echo $GAMENUM,$SECTIME,$PLAY >> pbpWsectime.csv
+done
+
+## get starters... somehow?
+## use playerID number
+## get subs from pbpWsectime.csv
+## GAMENUM,SECTIME,HOME1,HOME2,...,AWAY5
+## sub happens
+## GAMENUM,SECTIME,HOME1,HOME2,...,AWAY5
+
+
 
 ## shot list
 cat tmp.html | grep -B2 "mvc_sc_period=" \
@@ -76,5 +123,25 @@ for L in `seq 1 $HALFLINES`; do
 	DIST=`echo $TEXT | sed 's/ a dunk/ a dunk from 0 feet out/g' | sed 's/ dunks/ dunks from 0 feet out/g' | sed 's/.* from //g' | sed 's/ feet out.*//g'`
 	KEEPTEXT=`echo $SHOT | cut -d, -f1-6,8-10`
 
-	echo $GAMENUM,$KEEPTEXT,$DIST,$ASSIST,$ASSISTER,$BLOCK,$BLOCKER,$THREEPT,$DUNK,$LAYUP,$HOOK,$JUMPER,$TEXT >> shots.csv
+	#make seconds in column
+	HALF=`echo $SHOT | cut -d, -f5 | cut -c1`
+	TIME=`echo $SHOT | cut -d, -f6`
+	MIN=`echo $TIME | cut -d: -f1`
+	SEC=`echo $TIME | cut -d: -f2`
+	SECTENS=`echo $TIME | cut -d: -f2 | cut -c1`
+
+	if [[ SECTENS -eq 0 ]]; then
+		SEC=`echo $TIME | cut -d: -f2 | cut -c2`
+	fi
+
+
+	MININ=$((`echo $((19 - MIN))` * 60))
+	SECIN=$((60 - SEC))
+	### going to need some OT adj here
+	HALFADJ=$((`echo $((HALF - 1))` * 1200))
+
+	SECTIME=$((MININ + SECIN + HALFADJ))
+
+
+	echo $GAMENUM,$SECTIME,$KEEPTEXT,$DIST,$ASSIST,$ASSISTER,$BLOCK,$BLOCKER,$THREEPT,$DUNK,$LAYUP,$HOOK,$JUMPER,$TEXT >> shots.csv
 done
