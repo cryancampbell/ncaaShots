4396f0f3661b9c2565517997ef051860a1ffada3
Christopher Campbell <ryan@christosmacbook.lan>
2022-12-22 11:58:55 -0500

    started a shot plotting fxn

---
+++ plotShots.R
@@ -0,0 +1,8 @@
+plotShots <- function(allShots = shotDF, name = "Caleb Love", ID = NA, teamPlot = FALSE,
+                      plotType = "shots", lastNgames = "all", oppList = "all",
+                      colPalette = "blueRed", 
+                      saveDir = "~/Dropbox (Personal)/misc/bball_stats/") {
+  #allShots <- shotsDF
+  #plotType - shots, zones
+  
+}
